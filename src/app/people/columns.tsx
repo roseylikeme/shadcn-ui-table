73739e425b895099fa735156a799185c4cede3b3
Roselene Gabun <roselenegabun@gmail.com>
2024-01-19 07:08:39 -0800

    add actions column

--- src/app/people/columns.tsx
+++ src/app/people/columns.tsx
@@ -1,6 +1,18 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
+import { MoreHorizontal } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 
 export const columns: ColumnDef<Person>[] = [
     {
@@ -22,5 +34,31 @@ export const columns: ColumnDef<Person>[] = [
     {
         accessorKey: "date_of_birth",
         header: "Date of Birth",
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const person = row.original
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant='ghost' className='w-8 h-8 p-0'>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem
+                            onClick={() => {
+                                navigator.clipboard.writeText(person.email.toString())
+                            }}
+                        >
+                            Copy email
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        }
     }
 ]
