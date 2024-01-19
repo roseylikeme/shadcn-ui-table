846980fe1291a9ddf721c212bf839ef1495239cb
Roselene Gabun <roselenegabun@gmail.com>
2024-01-19 08:22:58 -0800

    sorting columns

--- src/app/people/columns.tsx
+++ src/app/people/columns.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
-import { MoreHorizontal } from "lucide-react"
+import { ArrowUpDown, MoreHorizontal } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -29,7 +29,17 @@ export const columns: ColumnDef<Person>[] = [
     },
     {
         accessorKey: "email",
-        header: "Email",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant='ghost'
+                    onClick={()=> column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Email
+                    <ArrowUpDown className="ml-2 h-4 w-4"/>
+                </Button>
+            )
+        }
     },
     {
         accessorKey: "date_of_birth",
--- src/app/people/data-table.tsx
+++ src/app/people/data-table.tsx
@@ -1,12 +1,15 @@
 "use client"
 
+import React from 'react'
 import { Button } from "@/components/ui/button"
 
 import {
     ColumnDef,
+    SortingState,
     flexRender,
     getCoreRowModel,
-    getPaginationRowModel, // new
+    getPaginationRowModel,
+    getSortedRowModel,
     useReactTable,
 } from "@tanstack/react-table"
 
@@ -18,8 +21,6 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
-   
-import React from 'react'
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[]
@@ -30,11 +31,19 @@ export function PeopleDataTable<TData, TValue>({
     columns,
     data
 }: DataTableProps<TData, TValue>) {
+    const [sorting, setSorting] = React.useState<SortingState>([])
+
     const table = useReactTable({
         data,
         columns,
         getCoreRowModel: getCoreRowModel(),
         getPaginationRowModel: getPaginationRowModel(), // new
+        onSortingChange: setSorting,
+        getSortedRowModel: getSortedRowModel(),
+        
+        state: {
+            sorting,
+        }
     })
 
     return (
