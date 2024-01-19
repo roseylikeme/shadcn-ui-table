1d8b017c6091f6a247546341c742143db79249c1
Roselene Gabun <roselenegabun@gmail.com>
2024-01-19 07:16:02 -0800

    add pagination

--- src/app/people/data-table.tsx
+++ src/app/people/data-table.tsx
@@ -1,9 +1,12 @@
-'use client'
+"use client"
+
+import { Button } from "@/components/ui/button"
 
 import {
     ColumnDef,
     flexRender,
     getCoreRowModel,
+    getPaginationRowModel, // new
     useReactTable,
 } from "@tanstack/react-table"
 
@@ -31,47 +34,68 @@ export function PeopleDataTable<TData, TValue>({
         data,
         columns,
         getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(), // new
     })
 
     return (
-        <div className="rounded-md border">
-            <Table>
-                <TableHeader>
-                {table.getHeaderGroups().map(headerGroup => {
-                        return (
-                            <TableRow key={headerGroup.id}>
-                                {headerGroup.headers.map(header => {
-                                    return (
-                                        <TableHead key={header.id}>
-                                            {flexRender(header.column.columnDef.header, header.getContext())}
-                                        </TableHead>
-                                    )
-                                })}
-                            </TableRow>
-                        )
-                    })}
-                </TableHeader>
+        <div>
+            <div className="rounded-md border">
+                <Table>
+                    <TableHeader>
+                    {table.getHeaderGroups().map(headerGroup => {
+                            return (
+                                <TableRow key={headerGroup.id}>
+                                    {headerGroup.headers.map(header => {
+                                        return (
+                                            <TableHead key={header.id}>
+                                                {flexRender(header.column.columnDef.header, header.getContext())}
+                                            </TableHead>
+                                        )
+                                    })}
+                                </TableRow>
+                            )
+                        })}
+                    </TableHeader>
 
-                <TableBody>
-                    {table.getRowModel().rows?.length ? (
-                        table.getRowModel().rows?.map(row => (
-                            <TableRow key={row.id}>
-                                {row.getVisibleCells().map(cell => (
-                                    <TableCell key={cell.id}>
-                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                    </TableCell>
-                                ))}
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows?.map(row => (
+                                <TableRow key={row.id}>
+                                    {row.getVisibleCells().map(cell => (
+                                        <TableCell key={cell.id}>
+                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell>
+                                    No results
+                                </TableCell>
                             </TableRow>
-                        ))
-                    ) : (
-                        <TableRow>
-                            <TableCell>
-                                No results
-                            </TableCell>
-                        </TableRow>
-                    )}
-                </TableBody>
-            </Table>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+            <div className="flex items-center justify-center space-x-2 py-4">
+                <Button
+                    variant='outline'
+                    size='sm'
+                    onClick={() => table.previousPage()}
+                    disabled={!table.getCanPreviousPage()}
+                >
+                    Previous
+                </Button>
+                <Button
+                    variant='outline'
+                    size='sm'
+                    onClick={() => table.nextPage()}
+                    disabled={!table.getCanNextPage()}
+                >
+                    Next
+                </Button>
+            </div>
         </div>
     )
 }
