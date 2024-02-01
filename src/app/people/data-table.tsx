dee7bdfa6bc4acc69c680fc8fb11e32bbde9b964
Roselene Gabun <roselenegabun@gmail.com>
2024-02-01 13:28:05 -0800

    add more pagination controls first & last page

--- src/app/people/data-table.tsx
+++ src/app/people/data-table.tsx
@@ -157,30 +157,50 @@ export function PeopleDataTable<TData, TValue>({
                 </Table>
             </div>
             <div>
-            <div>
-                <div className="flex items-center space-x-2 py-4">
-                <Button
-                    variant='outline'
-                    size='sm'
-                    onClick={() => table.previousPage()}
-                    disabled={!table.getCanPreviousPage()}
-                >
-                    Previous
-                </Button>
-                <Button
-                    variant='outline'
-                    size='sm'
-                    onClick={() => table.nextPage()}
-                    disabled={!table.getCanNextPage()}
-                >
-                    Next
-                </Button>
-                <div className='flex-1 text-sm text-muted-foreground'>
-                    {table.getFilteredSelectedRowModel().rows.length} of{" "}
-                    {table.getFilteredRowModel().rows.length} row(s) selected.
+                <div>
+                    <div className="flex items-center space-x-2 py-4">
+                        <div>
+                            <Button
+                                variant='outline'
+                                size='sm'
+                                onClick={() => table.setPageIndex(0)}
+                                disabled={!table.getCanPreviousPage()}
+                            >
+                                First
+                            </Button>
+                        </div>
+                        <Button
+                            variant='outline'
+                            size='sm'
+                            onClick={() => table.previousPage()}
+                            disabled={!table.getCanPreviousPage()}
+                        >
+                            Previous
+                        </Button>
+                        <Button
+                            variant='outline'
+                            size='sm'
+                            onClick={() => table.nextPage()}
+                            disabled={!table.getCanNextPage()}
+                        >
+                            Next
+                        </Button>
+                        <div className='flex-1 text-sm text-muted-foreground'>
+                            {table.getFilteredSelectedRowModel().rows.length} of{" "}
+                            {table.getFilteredRowModel().rows.length} row(s) selected.
+                        </div>
+                        <div>
+                            <Button
+                                variant='outline'
+                                size='sm'
+                                onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+                                disabled={!table.getCanNextPage()}
+                            >
+                                Last
+                            </Button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            </div>
             </div>
         </div>
     )
